c28604e92c4561c126e37772a46a9cd9b16ff3c3
Mubtasim Sawonto <mubtasimsawonto@Mubtasims-MBP.fios-router.home>
2020-08-15 15:19:52 -0400

    fooling around

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class HtmlManager:
         </html>"""
         newdoc = HtmlDocument(message)
         self.document = newdoc
-        print(newdoc)
+        print(newdoc) dawa
 
         
     def save_html_file(self):
